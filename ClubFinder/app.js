d553b75fb2228379e5be70799cc5ccce405c6f73
Malbamario <malba324@gmail.com>
2023-09-10 11:41:57 +0800

    Add Webpack to Club Finder

--- ClubFinder/app.js
+++ ClubFinder/app.js
@@ -2,5 +2,6 @@ import "./src/script/components/app-bar.js";
 import "./src/script/components/search-bar.js";
 import "./src/script/components/club-item.js";
 import "./src/script/components/club-list.js";
+import "./src/styles/style.css";
 import main from './src/script/view/main.js'
 document.addEventListener('DOMContentLoaded', main);
